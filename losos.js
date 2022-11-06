d6dd6aa8517e73acd0dee6e30a029cd50ad0e64a
Fedor <uasily.lol@yandex.ru>
2022-11-07 00:11:01 +0300

    adding slow cloud swim

--- losos.js
+++ losos.js
@@ -389,7 +389,7 @@
 let WidthWindow = 1920 + 310;
 const clouds = document.querySelectorAll(".cloud");
 let cloudLeft = null;
-let newcloudLeft = null;
+let newCloudLeft = null;
 let presenceAnimation = false;
 
 function flyingСlouds() {
@@ -397,14 +397,15 @@ function flyingСlouds() {
 		for (let cloud of clouds) {
 			cloudLeft = Math.round(cloud.getBoundingClientRect().left);
 			if (WidthWindow > cloudLeft) {
-				newcloudLeft = cloudLeft + 1;
-				cloud.style.transform = `translate(${newcloudLeft}px)`;
+				newCloudLeft = cloudLeft + 1;
+				cloud.style.transform = `translate(${newCloudLeft}px)`;
 			} else {
 				cloud.style.transform = `translate(-340px)`; 
 			}
 		}
 	}
+	requestAnimationFrame(flyingСlouds);
 }
-requestAnimationFrame(flyingСlouds);
+
 
 flyingСlouds()
